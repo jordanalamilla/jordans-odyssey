4576e488c4e711c1b4149973cb6728398c640709
Jordan Michael Alamilla <jordan.alamilla@gmail.com>
2025-12-07 17:53:48 -0500

    signature - increased size, typography - h4 size adjustment

--- footer.php
+++ footer.php
@@ -13,12 +13,12 @@ wp_footer();
 	<footer class="odd-footer">
 		<div class="container">
 			<div class="row">
-				<div class="col-12 col-md-2">
+				<div class="col-12 col-md-4">
 					<div class="sig sig-dark footer-sig"></div>
 				</div>
 
 				<!-- Icons -->
-				<div class="col-12 col-md-10">
+				<div class="col-12 col-md-8">
 					<?php get_template_part( 'template-parts/icons' ); ?>
 				</div>
 			</div>
